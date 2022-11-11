b0176295600aa90e6f16b4837803710967b3ca85
Grumbleur <github@grumbleur.fr>
2022-11-11 16:13:43 +0100

    tests: add tests for parser (#1)
    * tests: remove test redis not necessary
    
    * tests: create tests for parser

--- internal/app/parser/parser_test.go
+++ internal/app/parser/parser_test.go
@@ -1,1 +1,28 @@
-package parser_test
+package parser
+
+import (
+	"io/ioutil"
+	"strings"
+	"testing"
+)
+
+func TestGetFeedsFromRssParser(t *testing.T) {
+	xmlData := `
+	<rss>
+		<channel>
+			<item>
+				<title>Test item</title>
+				<link>this-is-a-test-link</link>
+			</item>
+		</channel>
+	</rss>`
+
+	reader := ioutil.NopCloser(strings.NewReader(xmlData))
+	feed, _ := getFeedsFromRssParser(reader)
+
+	t.Run("get link from items", func(t *testing.T) {
+		if feed.Items[0].Link != "this-is-a-test-link" {
+			t.Errorf("Expected link to be %s, got %s", "this-is-a-test-link", feed.Items[0].Link)
+		}
+	})
+}
